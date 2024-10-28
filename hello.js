7d52a88cfb19bf2304ab5d09e257064db4accea9
Shashank <shashankshekhar746@gmail.com>
2024-10-28 22:11:23 +0530

    Project Setup

---
+++ hello.js
@@ -0,0 +1,3 @@
+const math = require("./math")
+
+console.log(math.add(2,5),math.sub(9,7));
---
+++ math.js
@@ -0,0 +1,13 @@
+// const add = (a,b) => {
+//     return a+b;
+// }
+// const sub= (a,b) =>{
+//     return a-b;
+// }
+// module.exports = {
+//     add,
+//     sub,
+// };
+
+exports.add =(a,b) => a+b;
+exports.sub =(a,b) => a-b;
